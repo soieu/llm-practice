89864fb31415274653aba4169eee5d36944c63c0
soieu <102034740+soieu@users.noreply.github.com>
2024-10-26 23:03:08 +0900

    delete desktop

--- Desktop/llm-practice/chapter2/tokenid_to_vector.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-
-embedding_dim = 16 # 16차원의 벡터로 변환
-embed_layer = nn.Embedding(len(strToidx), embedding_dim) # 16차원의 임베딩을 생성하는 임베딩 층 생성
-
-input_embeddings = embed_layer(torch.tensor(input_ids) ) # 
-input_embeddings = input_embeddings.unsqueeze(0)
